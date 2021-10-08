27828b982bf8043dcdd38759296c8bdb684de50e
Baghram <baghram0001@gmail.com>
2021-10-08 14:59:25 +0700

    fix redis

--- helper/redis.js
+++ helper/redis.js
@@ -3,6 +3,7 @@ const { createClient } = require("redis");
 const client = createClient({
   url: process.env.REDIS,
   auth_pass: process.env.REDISPASS,
+  no_ready_check: true,
 });
 client.auth(process.env.REDISPASS);
 module.exports = client;
