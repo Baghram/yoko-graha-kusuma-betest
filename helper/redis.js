0f95515c33f9cc8d969945d1ae96e6d14c47f04d
Baghram <baghram0001@gmail.com>
2021-10-08 15:36:38 +0700

    redis

--- helper/redis.js
+++ helper/redis.js
@@ -1,9 +1,13 @@
 const { createClient } = require("redis");
 
-const client = createClient({
-  url: process.env.REDIS,
-  username: "default",
-  auth_pass: process.env.REDISPASS,
+const client = createClient(process.env.REDIS, {
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
-client.auth(process.env.REDISPASS);
+client['auth'] = null;
+client.on("error", (error) => {
+  console.log(error);
+});
+
 module.exports = client;
