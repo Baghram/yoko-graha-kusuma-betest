d93d131500af3ee4014e88d1f2f39956922c00d1
Baghram <baghram0001@gmail.com>
2021-10-08 15:52:28 +0700

    redis

--- helper/redis.js
+++ helper/redis.js
@@ -3,7 +3,6 @@ const { createClient } = require("redis");
 const client = createClient({
   url: process.env.REDIS,
 });
-client.sendCommand('AUTH', ["", process.env.REDISPASS])
 client.on("error", (error) => {
   console.log(error);
 });
