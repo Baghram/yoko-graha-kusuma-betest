bf5a40f265f69c13a44e3e6230778c51d97d7cfa
Baghram <baghram0001@gmail.com>
2021-10-08 14:57:21 +0700

    password

--- helper/redis.js
+++ helper/redis.js
@@ -2,7 +2,7 @@ const { createClient } = require("redis");
 
 const client = createClient({
   url: process.env.REDIS,
-  password: process.env.REDISPASS,
+  auth_pass: process.env.REDISPASS,
 });
 client.auth(process.env.REDISPASS);
 module.exports = client;
